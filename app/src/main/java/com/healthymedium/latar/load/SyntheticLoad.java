df6752eb3dfb7891688b51af7f9252be06c29934
Steven Huff <stephen@thinkhappymedium.com>
2020-05-15 12:33:58 -0500

    synthetic load - use thread count check

--- app/src/main/java/com/healthymedium/latar/load/SyntheticLoad.java
+++ app/src/main/java/com/healthymedium/latar/load/SyntheticLoad.java
@@ -14,6 +14,8 @@ public class SyntheticLoad {
         if(params.workload.isEmpty()){
             return;
         }
+        params.checkThreadCount();
+
         running = true;
 
         for(int i=0;i<params.threadCount;i++){
